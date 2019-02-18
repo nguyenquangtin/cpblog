bf9f2080b2b62db97202ae06c70d86c7edf0a285
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2019-02-18 14:01:12 +0700

    Add the global css and images

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
